b9c12c71f796c9674bb260a8e2d8344e272bf114
Daniel Bachniak <daniel.bachniak@ex7.pl>
2021-08-11 15:48:35 +0200

    add post request helper

---
+++ lib/requests/requests_helper.dart
@@ -0,0 +1,46 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class RequestsHelper {
+  static Future<List<T>> get<T>(
+    url,
+    T Function(Map<String, dynamic> jsonMap) fromMap,
+    //{
+    //String body = '',
+    //String token = ''
+    //}
+  ) async {
+    final response = await http.post(
+      Uri.parse(url),
+      headers: {"Content-Type": "application/json"},
+      //body: body,
+    ); // request
+    if (response.statusCode == 200) {
+      final parsed = jsonDecode(response.body)
+          .cast<Map<String, dynamic>>(); // parse to json
+      return parsed.map<T>((map) => fromMap(map)).toList(); // convert to list
+    } else {
+      throw Exception('Unable to get data from the API');
+    }
+  }
+
+  static Future<List<T>> post<T>(
+    url,
+    T Function(Map<String, dynamic> jsonMap) fromMap, {
+    String body = '',
+    //String token = ''
+  }) async {
+    final response = await http.post(
+      Uri.parse(url),
+      headers: {"Content-Type": "application/json"},
+      body: body,
+    ); // request
+    if (response.statusCode == 200) {
+      final parsed = jsonDecode(response.body)
+          .cast<Map<String, dynamic>>(); // parse to json
+      return parsed.map<T>((map) => fromMap(map)).toList(); // convert to list
+    } else {
+      throw Exception('Unable to get data from the API');
+    }
+  }
+}
