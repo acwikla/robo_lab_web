99182aff66ece73982a33042b691805078c2b744
Daniel Bachniak <daniel.bachniak@ex7.pl>
2021-08-24 23:33:24 +0200

    change side menu gradient

--- lib/widgets/side_menu.dart
+++ lib/widgets/side_menu.dart
@@ -32,12 +32,15 @@ class SideMenu extends StatelessWidget {
         gradient: LinearGradient(
           begin: Alignment.topCenter,
           end: Alignment.bottomCenter,
-          stops: [0.1, 0.5, 0.8, 0.9],
+          stops: [
+            0.1, 0.5, 0.9,
+            //0.9
+          ],
           colors: [
             topPanelColor,
             leftMenuColor,
             lighterleftMenuColor,
-            lightBlueGrey
+            //lightBlueGrey
           ],
         ),
       ),
