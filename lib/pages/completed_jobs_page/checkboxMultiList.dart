cbf853b39107393f351b01ac32ecd62b0aa36fd7
acwikla <acwikla.13@gmail.com>
2021-08-28 01:58:00 +0200

    change completed details page

--- lib/pages/completed_jobs_page/checkboxMultiList.dart
+++ lib/pages/completed_jobs_page/checkboxMultiList.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:robo_lab_web/constants/style_const.dart';
 import 'package:robo_lab_web/dto/view_device_value_dto.dart';
 import 'package:robo_lab_web/requests/device_jobs_requests.dart';
-import 'package:charts_flutter/flutter.dart' as charts;
 import 'package:syncfusion_flutter_charts/charts.dart';
 import '../../global.dart';
+import 'dart:async';
+import 'dart:html' as html;
 
 class MultiSeriesChartJobDetailsPage extends StatefulWidget {
   const MultiSeriesChartJobDetailsPage({Key? key}) : super(key: key);
@@ -45,11 +47,10 @@ class _MultiSeriesChartJobDetailsPageState
 
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return Row(
       children: [
-        Expanded(flex: 1, child: _buildTitle(context)),
-        Expanded(flex: 1, child: _buildCheckBoxList(context)),
-        Expanded(flex: 5, child: _buildMultiSeriesChartData(context)),
+        Expanded(flex: 1, child: _buildSelectChartDataArea(context)),
+        Expanded(flex: 3, child: _buildMultiSeriesChartData(context)),
       ],
     );
   }
@@ -128,56 +129,24 @@ class _MultiSeriesChartJobDetailsPageState
     });
   }
 
-  Widget _buildCheckBoxList(BuildContext context) {
+  Future getExelFile() async {
+    html.window.open(
+        Uri.encodeFull(
+            'http://51.158.163.165/api/device-jobs/${Global.deviceJob.id}/export-all-job-values'),
+        'open');
+  }
+
+  Widget _buildSelectChartDataArea(BuildContext context) {
     fetchPropertyNameValue();
     createChartSeries(_propNameValueList);
 
-    return Container(
-        decoration: BoxDecoration(
-          border: Border(
-            top: BorderSide(
-              color: lightBlueGrey.withOpacity(.6),
-              width: 1.5,
-            ),
-            bottom: BorderSide(
-              color: lightBlueGrey.withOpacity(.6),
-              width: 1.5,
-            ),
-          ),
-        ),
-        child: ListView.builder(
-          itemCount: _propNameList.length,
-          itemBuilder: (context, index) {
-            return Container(
-                child: Column(
-              children: [
-                new CheckboxListTile(
-                    activeColor: Colors.pink[300],
-                    dense: true,
-                    //font change
-                    title: new Text(
-                      _propNameList[index].name,
-                      style: TextStyle(
-                          fontSize: 14,
-                          fontWeight: FontWeight.w600,
-                          letterSpacing: 0.5),
-                    ),
-                    value: _propNameList[index].isCheck,
-                    onChanged: (bool? val) {
-                      itemChange(val!, index);
-                    })
-              ],
-            ));
-          },
-        ));
-  }
-
-  Widget _buildTitle(BuildContext context) {
-    return Container(
-        alignment: Alignment.topLeft,
-        padding: EdgeInsets.fromLTRB(10, 40, 30, 40),
-        child: ListView(children: [
-          Text(
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          margin: EdgeInsets.fromLTRB(20, 40, 10, 10),
+          child: Text(
             'Select chart data',
             style: TextStyle(
               fontFamily: 'Segoe UI',
@@ -186,8 +155,88 @@ class _MultiSeriesChartJobDetailsPageState
               fontWeight: FontWeight.bold,
             ),
           ),
-          SizedBox(height: 15),
-        ]));
+        ),
+        Expanded(
+          child: _buildCheckBoxList(context),
+        ),
+        Container(
+          margin: EdgeInsets.fromLTRB(20, 40, 10, 30),
+          child: _buildDownloadExcelButton(context),
+        )
+      ],
+    );
+  }
+
+  Widget _buildCheckBoxList(BuildContext context) {
+    return ListView.builder(
+      itemCount: _propNameList.length,
+      itemBuilder: (context, index) {
+        return Container(
+            margin: EdgeInsets.fromLTRB(20, 0, 0, 0),
+            decoration: BoxDecoration(
+              border: Border(
+                top: BorderSide(
+                  color: lightBlueGrey.withOpacity(.6),
+                  width: 1.5,
+                ),
+              ),
+            ),
+            child: Column(
+              children: [
+                new CheckboxListTile(
+                    activeColor: peachPuff,
+                    dense: true,
+                    title: new Text(
+                      _propNameList[index].name,
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontFamily: 'Segoe UI',
+                        //fontFamily: GoogleFonts.ptSansTextTheme.toString(),
+                        fontWeight: FontWeight.normal,
+                        //letterSpacing: 0.5
+                      ),
+                    ),
+                    value: _propNameList[index].isCheck,
+                    onChanged: (bool? val) {
+                      itemChange(val!, index);
+                    })
+              ],
+            ));
+      },
+    );
+  }
+
+  Widget _buildDownloadExcelButton(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          'Click button to download excel file with all the results.',
+          //'Download excel file with all the results.',
+          style: TextStyle(
+            color: darkerSteelBlue,
+            fontSize: 17,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
+          child: FloatingActionButton(
+            mini: true, //??
+            onPressed: () {
+              getExelFile();
+            },
+            child: Icon(
+              Icons.download_sharp,
+              size: 22,
+            ),
+            backgroundColor: peachPuff,
+            hoverColor: lightBlueGrey,
+            elevation: 5,
+            tooltip: 'Download excel file',
+          ),
+        )
+      ],
+    );
   }
 
   Widget _buildMultiSeriesChartData(BuildContext context) {
@@ -196,29 +245,26 @@ class _MultiSeriesChartJobDetailsPageState
         title: ChartTitle(
           text: 'Results of the completed job: ${Global.deviceJob.id}',
           alignment: ChartAlignment.far,
-          borderColor: Colors.transparent,
-          backgroundColor: Colors.transparent,
           textStyle: TextStyle(
             color: darkerSteelBlue,
             fontSize: 15,
             fontWeight: FontWeight.bold,
             decoration: TextDecoration.underline,
             decorationColor: lightBlueGrey,
-            decorationThickness: 2,
+            decorationThickness: 2.85,
             decorationStyle: TextDecorationStyle.solid,
           ),
         ),
-        margin: EdgeInsets.fromLTRB(10, 40, 10, 40),
-        //palette
+        margin: EdgeInsets.fromLTRB(30, 40, 10, 40),
         plotAreaBackgroundColor: superLightBlueGrey,
+        borderWidth: 2,
         legend: Legend(
             isVisible: true,
             position: LegendPosition.top,
             alignment: ChartAlignment.center,
-            //borderColor: lighterPeachPuff,
-            //borderWidth: 2,
             //offset: Offset(40, 40),
-            //overflowMode: LegendItemOverflowMode.wrap,
+            backgroundColor: Colors.white,
+            overflowMode: LegendItemOverflowMode.wrap,
             title: LegendTitle(
                 text: 'Data',
                 textStyle: TextStyle(
@@ -232,16 +278,17 @@ class _MultiSeriesChartJobDetailsPageState
           for (int i = 0; i < _propNameValueList.length; i++)
             LineSeries<ViewDeviceValueDto, DateTime>(
                 width: 2,
-                name: _propNameList[i].name,
-                color: leftMenuColor,
+                name: _propNameValueList[i][0].propertyName,
+                color: Colors.teal[400 + (i * 200)],
+                //.blueGrey[400 + (i * 200)], //leftMenuColor,
                 dataSource: _propNameValueList[i],
                 xValueMapper: (ViewDeviceValueDto deviceValueDto, _) =>
                     deviceValueDto.dateTime,
                 yValueMapper: (ViewDeviceValueDto deviceValueDto, _) =>
                     double.parse(deviceValueDto.value),
                 dataLabelSettings: DataLabelSettings(
-                    textStyle: TextStyle(
-                        fontSize: 14, color: Colors.blueGrey[400 + (i * 200)]),
+                    textStyle:
+                        TextStyle(fontSize: 14, color: Colors.blueGrey[400]),
                     isVisible: true),
                 enableTooltip: true),
         ],
@@ -249,7 +296,6 @@ class _MultiSeriesChartJobDetailsPageState
           edgeLabelPlacement: EdgeLabelPlacement.shift,
           //dateFormat: DateFormat.y(),
           labelStyle: TextStyle(color: Colors.black87, fontSize: 13),
-          //rangePadding: ,
           axisLine: AxisLine(color: lightBlueGrey),
           title: AxisTitle(
               text: 'Time',
